49a68490ab7879e5848bbe119c7057871ee1a606
Endrigo Carvalho <endrigorcarvalho@gmail.com>
2020-05-09 00:53:22 -0300

    Commit inicial. Lendo arquivo xml e inserindo na treeview

---
+++ ZylixForm/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ZylixForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(49, 69);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(258, 336);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(457, 203);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 147);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 434);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.treeView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ZylixForm/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ZylixForm
+{
+    public partial class Form1 : Form
+    {
+        private XmlDocument _xmlDocument;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _xmlDocument = new XmlDocument();
+            _xmlDocument.Load(@"C:\Users\endri\OneDrive\Documentos\Visual Studio 2015\Projects\Icon_Zylix\Zylix\ZylixForm\ConfiguracaoInicial.xml");
+            loadTreeView(_xmlDocument, treeView1);
+           
+        }
+
+        private void loadTreeView(XmlDocument xmlDoc, TreeView trv)
+        {
+            trv.Nodes.Clear();
+            AddTreeViewNode(trv.Nodes, xmlDoc.DocumentElement);
+        }
+        private void AddTreeViewNode(TreeNodeCollection parent_nodes, XmlNode xml_node)
+        {
+            if(xml_node.Attributes.GetNamedItem("Value") != null)
+            {
+                string valor = xml_node.Attributes.GetNamedItem("Value").InnerText;
+
+                TreeNode new_node = parent_nodes.Add(valor);
+
+                foreach (XmlNode child_node in xml_node.ChildNodes)
+                {
+                    AddTreeViewNode(new_node.Nodes, child_node);
+                }
+            }
+            
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string text = e.Node.Text;
+        }
+    }
+}
